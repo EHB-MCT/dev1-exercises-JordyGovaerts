2ec153d1dfdaed4153471820e3af72f9f1c3b1c1
Jordy Govaerts <jordy.govaerts@student.ehb.be>
2022-10-19 15:58:40 +0200

    exercise 3-1 i

--- exercises/3-iteration-while/3-1-i.js
+++ exercises/3-iteration-while/3-1-i.js
@@ -17,11 +17,11 @@ function drawMultipleConcentricCircles() {
     let i = 0;
     while (i <= 10) {
         let size = i * 15;
-        Utils.strokeCircle(200, 200, size);
-        Utils.strokeCircle(, , size);
-        Utils.strokeCircle(, , size);
-        Utils.strokeCircle(, , size);
-        Utils.strokeCircle(, , size);
-        i = ;
+        Utils.strokeEllipse(200, 200, size);
+        Utils.strokeEllipse(50,50 , size);
+        Utils.strokeEllipse(350,50 , size);
+        Utils.strokeEllipse(50,350 , size);
+        Utils.strokeEllipse(350,350 , size);
+        i++;
     }
 }
