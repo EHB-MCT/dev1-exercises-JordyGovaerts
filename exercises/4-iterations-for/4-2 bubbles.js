8fa097f4f15c466563c351d25f67fff3fd76d75c
Jordy Govaerts <jordy.govaerts@student.ehb.be>
2022-10-26 15:39:25 +0200

    Excercise 4-2 Random Coloured Bubbles

---
+++ exercises/4-iterations-for/4-2 bubbles.js
@@ -0,0 +1,23 @@
+"use strict"
+
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+drawBackground();
+strokeBubbles();
+
+function drawBackground(){
+    Utils.drawBackground(0,0,width,height);
+}
+function strokeBubbles() {
+
+for( let i = 0; i <= 90; i++ ) {
+let x = 200 + (Math.random() * (width-400));
+let y = 200 + (Math.random() * (height-400));
+let radius = Math.random() * 60;
+Utils.strokeBubbles(x,y,radius);
+} 
+}
--- scripts/utils.js
+++ scripts/utils.js
@@ -29,3 +29,25 @@ export function drawBox(x,y,width,height) {
     context.stroke();
     context.fill();
 }
+
+export function strokeBubbles(x1,y1,radius) {
+    let red = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let alpha = Math.random();
+    let color = "rgba(" + red + "," + blue + "," + green + "," + alpha + ")"
+    context.beginPath();
+    context.ellipse(x1,y1,radius,radius,0,0, 180);
+    context.fillStyle = color;
+    context.fill();
+    
+}
+
+export function drawBackground(x1,y1,x2,y2) {
+    context.beginPath();
+    context.rect(x1,y1,x2,y2);
+    context.fillStyle = "black";
+    context.stroke();
+    context.fill();
+
+}
