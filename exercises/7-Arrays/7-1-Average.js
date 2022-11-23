f21237dd00c2ee981080dd7e75d27d043abda2f9
Jordy Govaerts <jordy.govaerts@student.ehb.be>
2022-11-23 15:41:29 +0100

    7-1 Average

--- exercises/7-Arrays/7-1-Average.js
+++ exercises/7-Arrays/7-1-Average.js
@@ -5,23 +5,23 @@ import * as Utils from "../../scripts/utils.js";
 
 let width = context.canvas.width;
 let height = context.canvas.height;
-let Numbers = [];
 let AmountNumbers = 1000
 Setup();
 
 function Setup() {
+    let Numbers = [];
     for (let i = 0; i < AmountNumbers; i++) {
         Numbers[i] = Utils.randomNumber(0,100);
     }
-    console.log(Numbers);
-    calculateAverage();
+
+    console.log(calculateAverage(Numbers));
 }
 
-function calculateAverage(List) {
-let som = 0
-for (let i = 0; i < List.length; i++) {
-    let som = List.length(i);
-    
-    
+function calculateAverage(listofnumbers) {
+let sum = 0;
+for (let i = 0; i < listofnumbers.length; i++) {
+    sum += listofnumbers[i];  
 }
+
+return sum / listofnumbers.length;
 }
